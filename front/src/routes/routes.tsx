c13fd21c66d6ee4a35c20bde58ef3babb44de62d
Castelo <90470038+Pedr0Castel0@users.noreply.github.com>
2024-09-13 17:19:38 -0300

    Revert "feat: Adicionar navegação para "Meus Reportes" na página inicial"
    This reverts commit 3909afe706852f55ef49334f4529b198a94f479d.

--- front/src/routes/routes.tsx
+++ front/src/routes/routes.tsx
@@ -2,7 +2,6 @@ import Home from '@pages/Home';
 import Landing from '@pages/Landing';
 import Report from '@pages/Report';
 import ViewReport from '@pages/ViewReport';
-import MyReports from '@pages/ViewReport/myReports';
 import { createBrowserRouter } from 'react-router-dom';
 import PrivateLayout from '../layouts/PrivateLayout';
 
@@ -13,7 +12,6 @@ export const AppRoutes = createBrowserRouter([
       { path: '/home', element: <Home /> },
       { path: '/reporte/:id', element: <ViewReport /> },
       { path: '/reporte', element: <Report /> },
-      { path: '/meus-reportes', element: <MyReports /> },
     ],
   },
   { path: '/', element: <Landing /> },
