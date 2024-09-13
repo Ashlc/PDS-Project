3909afe706852f55ef49334f4529b198a94f479d
Castelo <90470038+Pedr0Castel0@users.noreply.github.com>
2024-09-13 16:25:18 -0300

    feat: Adicionar navegação para "Meus Reportes" na página inicial

--- front/src/routes/routes.tsx
+++ front/src/routes/routes.tsx
@@ -2,6 +2,7 @@ import Home from '@pages/Home';
 import Landing from '@pages/Landing';
 import Report from '@pages/Report';
 import ViewReport from '@pages/ViewReport';
+import MyReports from '@pages/ViewReport/myReports';
 import { createBrowserRouter } from 'react-router-dom';
 import PrivateLayout from '../layouts/PrivateLayout';
 
@@ -12,6 +13,7 @@ export const AppRoutes = createBrowserRouter([
       { path: '/home', element: <Home /> },
       { path: '/reporte/:id', element: <ViewReport /> },
       { path: '/reporte', element: <Report /> },
+      { path: '/meus-reportes', element: <MyReports /> },
     ],
   },
   { path: '/', element: <Landing /> },
