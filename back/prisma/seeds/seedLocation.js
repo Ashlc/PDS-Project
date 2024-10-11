6bec086a3678a177ea60e8b1854b0c37692ba52a
Carlos Eduardo N. T. Galvao <52842385+cedugalvao@users.noreply.github.com>
2024-10-11 12:47:01 -0300

    :bug: fix: seed randomizer removed

--- back/prisma/seeds/seedLocation.js
+++ back/prisma/seeds/seedLocation.js
@@ -1,49 +1,20 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
+
 const prisma = new PrismaClient();
-const { faker } = require('@faker-js/faker');
 
 async function seedLocation() {
-    // Localização original
-    const originalLocation = {
-        id: 1,
-        address: "Avenida Fernandes Lima, Maceió - AL",
-        complement: "Próximo ao McDonald's",
-        latitude: -9.648921,
-        longitude: -35.717239
-    };
-
-    // Adiciona a localização original
-    await prisma.location.upsert({
-        where: { id: originalLocation.id },
+    const location = await prisma.location.upsert({
+        where: { id: 1 },
         update: {},
-        create: originalLocation
+        create: {
+            id: 1,
+            address: "Avenida Fernandes Lima, Maceió - AL",
+            complement: "Próximo ao McDonald's",
+            latitude: -9.648921,
+            longitude: -35.717239,
+        },
     });
-
-    // Gerando 5 novas localizações aleatórias
-    const newLocations = [];
-    for (let i = 0; i < 5; i++) {
-        newLocations.push({
-            id: faker.number.int({ min: 2, max: 1000 }), // Gera IDs únicos aleatórios
-            address: faker.location.streetAddress(),
-            complement: faker.location.secondaryAddress(),
-            latitude: parseFloat(faker.location.latitude()),
-            longitude: parseFloat(faker.location.longitude())
-        });
-    }
-
-    for (const locationData of newLocations) {
-        await prisma.location.upsert({
-            where: { id: locationData.id },  // Use o campo id que é @id
-            update: {},
-            create: locationData
-        });
-    }
-
-    // Log para verificar as localizações criadas
-    const allLocations = await prisma.location.findMany();
-    console.log('Localizações criadas:', allLocations);
-
-    console.log('Locations populated');
+    return location;
 }
 
 module.exports = { seedLocation };
--- back/prisma/seeds/seedUser.js
+++ back/prisma/seeds/seedUser.js
@@ -1,41 +1,18 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
+
 const prisma = new PrismaClient();
-const { faker } = require('@faker-js/faker');
 
 async function seedUser() {
-    // Usuário original
-    const originalUser = {
-        email: "teste@teste.com",
-        password: "$2b$10$TUkRm4/YIOcFYOyKZhdo.2hPVdo/XZZ/.NJIfGQpcd2CxgXO5ngC", // senha 123
-        role: "admin"
-    };
-
-    // Adiciona o usuário original
-    await prisma.user.upsert({
-        where: { email: originalUser.email },
+    const user = await prisma.user.upsert({
+        where: { email: "teste@teste.com" },
         update: {},
-        create: originalUser
+        create: {
+            email: "teste@teste.com",
+            password: "$2b$10$5TUkRm4/YI0cFYOyKZhdo.2hPVdO/XZZ/.NJIfGQpcd2CXgXO5ngC", // 123
+            role: "admin",
+        },
     });
-
-    // Gerando 5 novos usuários aleatórios
-    const newUsers = [];
-    for (let i = 0; i < 5; i++) {
-        newUsers.push({
-            email: faker.internet.email(),
-            password: faker.internet.password(),
-            role: faker.helpers.arrayElement(['admin', 'user']),
-        });
-    }
-
-    for (const userData of newUsers) {
-        await prisma.user.upsert({
-            where: { email: userData.email },
-            update: {},
-            create: userData
-        });
-    }
-
-    console.log('Users populated');
+    return user;
 }
 
 module.exports = { seedUser };
